39bf63616a6f00a7ae641b7b1dbf390469a88ac4
Md. Shamim Rahman <apubdtea@gmail.com>
2025-08-09 09:39:42 +0600

    feat: add schedular initial func

---
+++ schedular/schedular.py
@@ -0,0 +1,5 @@
+def get_free_slots():
+    """Return list of available start times for a given working day, avoiding overlapping with existing bookings."""
+    free_slots = []
+
+    return free_slots
