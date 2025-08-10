b07a38200877b4b33302f9966f15485c333dece4
Md. Shamim Rahman <apubdtea@gmail.com>
2025-08-10 10:43:18 +0600

    import: helper functions in schedular

--- schedular/schedular.py
+++ schedular/schedular.py
@@ -1,3 +1,6 @@
+from .time_utils import parse_time, add_minute, overlaps
+
+
 def get_free_slots():
     """Return list of available start times for a given working day, avoiding overlapping with existing bookings."""
     free_slots = []
